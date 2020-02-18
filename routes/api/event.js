e77d93f2852c54a614f39abbc6a1b627b73264b1
Ben Florentino <florentinob92@gmail.com>
2020-02-17 22:48:39 -0500

    chore: Raw coded Routes / Handlebars

--- routes/api/event.js
+++ routes/api/event.js
@@ -1,4 +1,39 @@
 var router = require("express").Router();
 var Event = require("../../models/event");
 
+router.get('/', async (req, res) => {
+    const events = await Event.all();
+    res.json({ events: events });
+});
+
+router.post('/', async (req, res) => {
+    const result = await Event.create(req.body);
+    res.json(result);
+});
+
+router.get('/:id', async (req, res) => {
+    const preference = await Event.getById(req.params.id);
+    res.json(preference);
+});
+
+router.put('/:id', async (req, res) => {
+    const result = await Event.update(req.params.id, req.body);
+    res.json(result);
+});
+
+router.delete('/:id', async (req, res) => {
+    const result = await Event.delete(req.params.id);
+    res.json(result);
+});
+
+router.get('/:id/preferences', async (req, res) => {
+    const result = await Event.preferences(req.params.id);
+    res.json(result);
+});
+
+router.post('/:id/preferences', async (req, res) => {
+    const result = await Event.addPreference(req.params.id, req.body);
+    res.json(result);
+});
+
 module.exports = router;
--- routes/api/preferences.js
+++ routes/api/preferences.js
@@ -1,4 +1,29 @@
 var router = require("express").Router();
-var Preferences = require("../../models/preferences");
+var Preference = require("../../models/preferences");
+
+router.get('/', async (req, res) => {
+    const preferences = await Preference.all();
+    res.json({ preferences: preferences }); 
+});
+
+router.post('/', async (req, res) => {
+    const result = await Preference.create(req.body);
+    res.json(result);
+});
+
+router.get('/:id', async (req, res) => {
+    const preference = await Preference.getById(req.params.id);
+    res.json(preference);
+});
+
+router.put('/:id', async (req, res) => {
+    const result = await Preference.update(req.params.id, req.body);
+    res.json(result);
+});
+
+router.delete('/:id', async (req, res) => {
+    const result = await Preference.delete(req.params.id);
+    res.json(result);
+});
 
 module.exports = router;
