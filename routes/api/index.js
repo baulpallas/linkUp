00178c667c65b3a4e3a4f2ca75913eb21015b6cb
Paul Ballas <paballas51@gmail.com>
2020-02-18 15:05:57 -0500

    chore: change preferences table to have lat and lng columns

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,7 +1,9 @@
 var router = require("express").Router();
 
 router.use("/creator", require("./creator"));
+
 router.use("/event", require("./event"));
+
 router.use("/preferences", require("./preferences"));
 
 module.exports = router;
