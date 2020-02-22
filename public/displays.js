b2ef907063811dedb1c635a231ea3ed4e080105b
Pamela Vasquez <pamelavasquez94@gmail.com>
2020-02-22 12:38:37 -0500

    chores: fixed all route links

--- public/displays.js
+++ public/displays.js
@@ -20,18 +20,18 @@ $("#submitbtn").click(function() {
   $(".host-home").css("display", "block");
   $(".newhost").css("display", "none");
 });
-// $("#log-in-btn").click(function() {
-//   $(".host-home").css("display", "block");
-//   $(".exisitnghost").css("display", "none");
-// });
+$("#log-in-btn").click(function() {
+  $(".host-home").css("display", "block");
+  $(".exisitnghost").css("display", "none");
+});
 // $("#new-event-btn").click(function() {
 //   $(".new-event").css("display", "block");
 //   $(".host-home").css("display", "none");
 // });
-// $("#submit-event-btn").click(function() {
-//   $(".new-event").css("display", "none");
-//   $(".event-id").css("display", "block");
-// });
+$("#submit-event-btn").click(function() {
+  $(".new-event").css("display", "none");
+  $(".event-id").css("display", "block");
+});
 // $(".enter-pref").click(function() {
 //   $(".event-id").css("display", "none");
 //   $(".guest").css("display", "none");
