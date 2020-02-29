2f635c0df1048cecfaae156606a69d1a455127a8
Paul Ballas <paballas51@gmail.com>
2020-02-28 22:21:18 -0500

    chore: getting app to work with heroku

--- config/index.js
+++ config/index.js
@@ -1,7 +1,8 @@
 var Sequelize = require("sequelize");
 
 const sequelize = new Sequelize(
-  process.env.DATABASE_URL || "mysql://root:@localhost:3306/meetup_db"
+  process.env.DATABASE_URL ||
+    "mysql://a42p3imwul29ukd2:v9z415yli71v1m1b@ryvdxs57afyjk41z.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/qk0it2sq8unsr7rl"
 );
 
 module.exports = sequelize;
